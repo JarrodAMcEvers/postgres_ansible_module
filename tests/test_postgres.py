9f86dc5500ed08b7bd6e46fffb6d2154aa446104
Jarrod McEvers <jarrodamcevers@gmail.com>
2018-08-25 21:45:59 -0500

    Refactored common test setup

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -10,47 +10,32 @@ class TestPostgresHandler(unittest.TestCase):
 
     def setUp(self):
         self.postgresHandler = PostgresHandler()
+
         psycopg2.connect = MagicMock()
 
-    def testAnsibleModuleIsCreated(self):
-        module = MagicMock()
-        module.exit_json = MagicMock()
-        basic.AnsibleModule = MagicMock(return_value=module)
+        self.module = MagicMock()
+        self.module.params = {
+            'database': fake.name(),
+            'host': fake.name(),
+            'port': fake.random_number(4),
+            'user': fake.name(),
+            'password': fake.name(),
+            'query': fake.name()
+        }
+        self.module.exit_json = MagicMock()
+        basic.AnsibleModule = MagicMock(return_value=self.module)
 
+    def testAnsibleModuleIsCreated(self):
         main()
 
         basic.AnsibleModule.assert_called_with(argument_spec=self.postgresHandler.fields)
 
     def testConnectsToPostgresDatabaseWithParams(self):
-        module = MagicMock()
-        module.params = {
-            'database': fake.name(),
-            'host': fake.name(),
-            'port': fake.random_number(4),
-            'user': fake.name(),
-            'password': fake.name(),
-            'query': fake.name()
-        }
-        module.exit_json = MagicMock()
-        basic.AnsibleModule = MagicMock(return_value=module)
-        psycopg2.connect = MagicMock()
-
         main()
 
-        psycopg2.connect.assert_called_with(host=module.params['host'], database=module.params['database'], user=module.params['user'], password=module.params['password'], port=module.params['port'])
+        psycopg2.connect.assert_called_with(host=self.module.params['host'], database=self.module.params['database'], user=self.module.params['user'], password=self.module.params['password'], port=self.module.params['port'])
 
     def testExecutesTheQueryGiven(self):
-        module = MagicMock()
-        module.params = {
-            'database': fake.name(),
-            'host': fake.name(),
-            'port': fake.random_number(4),
-            'user': fake.name(),
-            'password': fake.name(),
-            'query': fake.name()
-        }
-        module.exit_json = MagicMock()
-        basic.AnsibleModule = MagicMock(return_value=module)
         connection = MagicMock()
         cursor = MagicMock()
         cursor.execute = MagicMock()
@@ -60,4 +45,4 @@ class TestPostgresHandler(unittest.TestCase):
         main()
 
         connection.cursor.assert_called()
-        cursor.execute.assert_called_with(module.params['query'])
+        cursor.execute.assert_called_with(self.module.params['query'])
